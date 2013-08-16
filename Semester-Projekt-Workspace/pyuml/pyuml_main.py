8a9acfb20914840356b9880d67d4495b80e87dad
twall <tomwallroth@gmail.com>
2013-08-16 17:27:06 +0200

    generator creates DAOs now

--- Semester-Projekt-Workspace/pyuml/pyuml_main.py
+++ Semester-Projekt-Workspace/pyuml/pyuml_main.py
@@ -26,20 +26,33 @@ import pyumljava
 tree = pyuml.UMLParser(UML_FILE)
 umljava = pyumljava.PyUMLJava(tree, debug=True)
 
+def mkdir_ignore(dirname):
+    try:
+        os.makedirs(dirname)
+    except OSError:
+        pass #dir already exists
 
 #print('creating package structure and')
 #print('creating files from templates')
 from bottle import template
 for packagename, pkgcontent in umljava.packages.items():
+    # create POJOS
     pkgdir = os.path.join(OUTPUT_FOLDER, packagename.replace('.','/'))
-    try:
-        os.makedirs(pkgdir)
-    except OSError:
-        pass #dir already exists
+    mkdir_ignore(pkgdir)
     everything_in_package = pkgcontent['classes']+pkgcontent['interfaces']+pkgcontent['enums']
     for e in everything_in_package:
         rendered = template('java', template_lookup=['templates'], e=e)
         filename = e.name+'.java'
         with open(os.path.join(pkgdir, filename), 'w') as javafile:
             javafile.write(rendered)
+    
+    # create DAOS
+    daopackage = '.'.join(packagename.split('.')[:-1])+'.daos'
+    daopkgdir = os.path.join(OUTPUT_FOLDER, daopackage.replace('.','/'))
+    mkdir_ignore(daopkgdir)
+    for cls in pkgcontent['classes']:
+        rendered = template('dao', template_lookup=['templates'], cls=cls, daopackage=daopackage)
+        filename = cls.name+'DAO.java'
+        with open(os.path.join(daopkgdir, filename), 'w') as daofile:
+            daofile.write(rendered)
 
---
+++ Semester-Projekt-Workspace/pyuml/templates/dao.tpl
@@ -0,0 +1,29 @@
+package {{daopackage}};
+
+import java.util.List;
+
+import {{cls.package}}.{{cls.name}};
+
+public class {{cls.name}}DAO extends ADAO {
+
+	public {{cls.name}} create{{cls.name}}(final {{cls.name}} {{cls.name.lower()}}) {
+		return create({{cls.name.lower()}});
+	}
+
+	public {{cls.name}} read{{cls.name}}(final long id) {
+		return read({{cls.name}}.class, id);
+	}
+
+	public {{cls.name}} update{{cls.name}}(final {{cls.name}} {{cls.name.lower()}}) {
+		return update({{cls.name.lower()}});
+	}
+
+	public List<{{cls.name}}> readAll{{cls.name}}s() {
+		return readByJPQL("select t from {{cls.name}} t");
+	}
+
+	public void delete{{cls.name}}(final {{cls.name}} {{cls.name.lower()}}) {
+		delete({{cls.name.lower()}});
+	}
+
+}
